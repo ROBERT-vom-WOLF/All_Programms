75310fd50852cddcd093b9e7a0d015fcd65e8c27
ROBERT-vom-WOLF <raid.scorbius@gmail.com>
2023-09-29 09:39:38 +0200

    commit

---
+++ Peter_Python_Kurs/banker_roulette_peter.py
@@ -0,0 +1,11 @@
+import random
+print("Wer soll heute zahlen?")
+print("Bitte beachten sie, dass sie alle Personen mit einem "," trennen.\n ")
+eingabe = input("Namen:\t")
+names = eingabe.split(",")
+names.append(eingabe)
+zahler = random.choices(names)
+zahler_str = str(zahler)
+letters = zahler_str.count("")
+zahler_str = zahler_str[2:(letters - 3)]
+print(f"Es ist: {zahler_str}")
